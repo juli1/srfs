2b7d9d3ac0052fb2658c602fecfb3aa92b7d96d8
Julien Delange <julien@gunnm.org>
2016-12-06 16:47:55 -0500

    improve test case

--- server/tests/test_client.cpp
+++ server/tests/test_client.cpp
@@ -11,6 +11,16 @@ BOOST_AUTO_TEST_CASE(check_readrequest)
 {
 	request r;
 
+	BOOST_CHECK (read_request ("aaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbb", r) == invalid_request);
+
 	BOOST_CHECK (read_request ("b", r) == invalid_request);
+
 	BOOST_CHECK (read_request ("open bla", r) == no_error);
+	BOOST_CHECK (r.getType() == openfile);
+
+	BOOST_CHECK (read_request ("close 0", r) == no_error);
+	BOOST_CHECK (r.getType() == closefile);
+
+	BOOST_CHECK (read_request ("read 0 100", r) == no_error);
+	BOOST_CHECK (r.getType() == readfile);
 }
